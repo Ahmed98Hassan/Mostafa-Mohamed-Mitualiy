da1a0f5f2c1a04559f24bd86ddc55476da23ca01
unknown <ahmedhassan4745@gmail.com>
2022-09-21 20:22:14 +0200

    upload

---
+++ js/cv.js
@@ -0,0 +1,39 @@
+// header //
+let maker = document.querySelector('#maker');
+let list = document.querySelectorAll('ul li');
+function moveindicator (e){
+    maker.style.left = e.offsetLeft + 'px';
+    maker.style.width = e.offsetWidth+ 'px'
+}
+list.forEach(link => {
+    link.addEventListener('mousemove' , (e) =>{
+        moveindicator(e.target);
+    })
+})
+// add active class in hovered list item 
+function activeLink(){
+    list.forEach((item)=> 
+    item.classList.remove('activat'));
+    this.classList.add('activat');
+}
+list.forEach((item)=>
+item.addEventListener('mouseover', activeLink))
+
+// Certificates //
+let card = document.getElementsByClassName('card');
+        function ss(i){
+            card[i].classList.toggle('active');
+        }; 
+
+
+
+// test
+var x = document.getElementById('x');
+function number(){
+    x.classList.toggle('num');
+}
+// gmail 
+let g = document.querySelector('#gmail');
+function gmail(){
+    g.classList.toggle('gmail')
+}
